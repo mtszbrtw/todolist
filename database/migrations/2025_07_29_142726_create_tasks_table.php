ee7997cab7b8a4ee4bbb20a8ca842d3ef26ca808
Mateusz Bartków <mtszbrtw@gmail.com>
2025-07-29 16:32:43 +0200

    Dodano nową migrację: tasks / tak taprawde to test gita

---
+++ database/migrations/2025_07_29_142726_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
+            $table->enum('status', ['to-do', 'in progress', 'done'])->default('to-do');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+
+        Schema::create('task_shares', function(Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id')->constrained()->onDelete('cascade');
+            $table->string('token')->unique();
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('task_shares');
+        Schema::dropIfExists('tasks');
+    }
+};
